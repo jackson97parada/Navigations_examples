d9bf163dd16680186c87ae62c2fcec156d9c1ff6
jackson Arevalo <jackson97parada@gmail.com>
2022-11-17 14:29:42 -0500

    New landing Navigations TAILWIND

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     fontFamily: {
       DM: ["DM Sans", "sans-serif"],
+      Poppin: ["Poppins", "sans-serif"],
     },
     extend: {
       colors: {
         blueDark: "#2F1893",
+        blueBlack: "#1E0E62",
       },
     },
   },
-  plugins: [],
+  safelist: [
+    "w-64",
+    "w-1/2",
+    "rounded-l-lg",
+    "rounded-r-lg",
+    "bg-gray-200",
+    "grid-cols-4",
+    "grid-cols-7",
+    "h-6",
+    "leading-6",
+    "h-9",
+    "leading-9",
+    "shadow-lg",
+  ],
+  plugins: [
+    // include Flowbite as a plugin in your Tailwind CSS project
+    require("flowbite/plugin"),
+  ],
 };
